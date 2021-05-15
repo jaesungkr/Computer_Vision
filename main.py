9d701af5dbba217bf44a2db3674ad4cfba736d6b
jaesungkr <kr.jaesung@gmail.com>
2021-05-15 23:28:38 +0900

    update

--- main.py
+++ main.py
@@ -1,47 +1,79 @@
-import numpy as np
-import cv2
+import numpy as np, cv2
 
-cap = cv2.VideoCapture('./videos/walking.avi')
+vid = cv2.VideoCapture('./videos/mine.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+writer = cv2.VideoWriter('output.avi', fourcc, 30.0, (int(vid.get(3)), int(vid.get(4))))
+delay = int(1000 / 15)  # slows the video
+# random color_forLines only for lines
+color_forLines = np.random.randint(0, 255, (200, 3))
+lines = None
+previous = None  # previous img
+blackscreen = 0 # black screen variable(button)
+#feature paramter for goodfeaturestotrack function
+feature_params = dict( maxCorners = 25,qualityLevel = 0.01,minDistance = 10,blockSize = 8)
 
-feature_params = dict(maxCorners = 100,
-                      qualityLevel = 0.3,
-                      minDistance = 7,
-                      blockSize =7)
+# calcOpticalFlowPyrLK termination
+termi = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 15, 0.08)
 
-lk_params = dict(winSize = (15,15),
-                 maxLevel = 2,
-                 criteria = (cv2.TermCriteria_EPS | cv2.TERM_CRITERIA_COUNT, 10,0.03))
+while True:
+    ret, frame = vid.read()
+    if ret == False:
+        exit()
+    Drawing = frame.copy()
+    #change to gray img
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # if first image
+    if previous is None:  #if theres no image before, meaning first
+        previous = gray
+        # image frame with np.zeros
+        lines = np.zeros_like(frame)
+        # find corner
+        mask = np.zeros_like(previous)
+        mask[:] = 300
+        prevPt = cv2.goodFeaturesToTrack(previous,mask=mask, **feature_params)
 
-color = np.random.randint(0,255,(100,3))
+    else:  #after the first image
+        nextImg = gray
+        # opticalflow to search next point and calculate
+        nextPt, status, err = cv2.calcOpticalFlowPyrLK(previous, nextImg, prevPt, None, criteria=termi)
+        # find moved points
+        #delete 0 from 0,1 for good points
+        ##print(prevPt)
+        pre_mov = prevPt[status != 0]
+        nex_mov = nextPt[status != 0]
+        if blackscreen == 1:
+            Drawing = np.zeros((int(vid.get(4)), int(vid.get(3)), 3), np.uint8)
+        for i, (p, n) in enumerate(zip(pre_mov, nex_mov)):
+            px, py = p.reshape(-1)
+            nx, ny = n.reshape(-1)
+            # draw line between previous and current points
+            cv2.line(lines, (int(px), int(py)), (int(nx), int(ny)), color_forLines[i].tolist(), 2)
+            # plot circle on new points
+            cv2.circle(Drawing, (int(nx), int(ny)), 2, (0, 255, 0), -1)
 
-ret,old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+        # add to img-draw
+        Drawing = cv2.addWeighted(Drawing, 0.8, lines, 0.3, 0.1)
+        # store current img to previous variable
 
-mask = np.zeros_like(old_frame)
+        previous = nextImg
+        prevPt = nex_mov.reshape(-1, 1, 2)
 
-while(1):
-    res, frame = cap.read()
-    frame_grey = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-
-    p1,st,err = cv2.calcOpticalFlowPyrLK(old_gray,frame_grey,p0,None,**lk_params)
-
-    good_new = p1[st==1]
-    good_old = p0[st==1]
-
-    for i, (new,old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask,(a,b), (c,d), color[i].tolist(),2)
-        frame = cv2.circle(frame,(a,b), 5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
-    cv2.imshow('frame', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    #draw
+    cv2.imshow('Jaesung Park', Drawing)
+    #recording
+    writer.write(Drawing)
+    key = cv2.waitKey(delay)
+    #esc button to quit
+    if key == 27:
         break
-
-    old_grey = frame_grey.copy()
-    p0 = good_new.reshape(-1,1,2)
+    elif key == ord('b'): # b to screen black
+        if blackscreen == 0:
+            blackscreen = 1
+        else:
+            blackscreen = 0
+    elif key == ord('d'): # d to delete previous lines for more clear view
+        previous = None
 
 cv2.destroyAllWindows()
-cap.realease()
+vid.release()
+writer.release()
